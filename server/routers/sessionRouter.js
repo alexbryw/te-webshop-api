3f4539bed09c7f0a6debf6e81829b5550ccc600b
Fredrik Malmborg <fredrik.malmborg@medieinstitutet>
2020-05-25 11:21:37 +0200

    moved files

--- server/routers/sessionRouter.js
+++ server/routers/sessionRouter.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 //const {getUsers, getUser} = require('../middleware/user/user-mw')
-const secureRoute = require('../middleware/sessionAuth/secure-mw')
-const {login, checkLogin, logout} = require('../middleware/sessionAuth/login')
+const checkLoginSession = require('../middlewares/sessionAuth/checkLoginSession')
+const {login, checkLogin, logout} = require('../middlewares/sessionAuth/login')
 
 //checks if user is loggedIn
-router.get('/api/login', secureRoute,checkLogin)
+router.get('/api/login', checkLoginSession,checkLogin)
 
 //logIn.
 router.post('/api/login', login)
