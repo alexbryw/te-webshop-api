e8e6adad3e80a960e104a6368bcb382cb73b92db
Fredrik Malmborg <fredrik.malmborg@medieinstitutet>
2020-05-25 11:20:43 +0200

    added createUser

--- server/middlewares/user/user-mw.js
+++ server/middlewares/user/user-mw.js
@@ -1,18 +1,39 @@
+
+const User = require("../../models/userModel");
+
+
 //Get all users from database..
-function getUsers(req, res, next) {
-    console.log("from get users")
-    res.json({msg: "Here are all users."})
-    // new Error(message)
-    // next(Error)
-    next()
+async function getUsers(req, res, next) {
+    try {
+        const users = await User.find();
+        res.json(users);
+        next()
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 }
 
 //Get one user by id from db.
 function getUser(req, res, next) {
-    //get att users from database.
     console.log("from one users")
-    res.json({msg: "Here is one(1) user."})
+    res.json({ msg: "Here is one(1) user." })
     next()
 }
 
-module.exports = {getUsers, getUser}
+async function createUser(req, res, next) {
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        admin: req.body.admin,
+    });
+    try {
+        const newUser = await user.save();
+        res.status(201).json({ name: newUser.name, admin: newUser.admin });
+        next()
+    } catch (err) {
+        // res.status.apply(400).json({ message: err.message })
+        res.status(400).json({ message: err.message });
+    }
+}
+
+module.exports = { getUsers, getUser, createUser }
