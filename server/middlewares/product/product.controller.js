77b723d3cebf2a705c1166292764f0c426128acc
Markus Karlsson <markuskarlsson__@hotmail.com>
2020-05-26 16:14:04 +0200

    code cleaning

--- server/middlewares/product/product.controller.js
+++ server/middlewares/product/product.controller.js
@@ -1,66 +1,24 @@
 const Product = require("../../models/productModel");
 
 function getAllProducts(req, res, next) {
-  // res.json({ msg: "hej då get" })
   Product.find({}, (err, result) => {
     if (err) {
-      next(err)
+      next(err);
     } else {
-      res.json(result)
+      res.json(result);
     }
-  })
+  });
 }
 
 function addProduct(req, res, next) {
-  const product = new Product(req.body)
+  const product = new Product(req.body);
   product.save((err, product) => {
     if (err) {
-      next(err)
+      next(err);
     } else {
-      res.json(product)
+      res.json(product);
     }
-  })
+  });
 }
-
-// // Get all products from database
-// async function getAllProducts(req, res, next) {
-//   try {
-//     const products = await Product.find();
-//     res.json(products);
-//     next();
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// }
-
-// // Get one product by id from database
-// function getProduct(req, res, next) {
-//   res.json();
-// }
-
-// async function addProduct(req, res, next) {
-//   const product = new Product({
-//     title: req.body.title,
-//     description: req.body.description,
-//     price: req.body.price,
-//     category: req.body.category,
-//     nrInStock: req.body.nrInStock,
-//   });
-//   try {
-//     const newProduct = await product.save();
-//     res
-//       .status(201)
-//       .json({
-//         title: newProduct.title,
-//         description: newProduct.description,
-//         price: newProduct.price,
-//         category: newProduct.category,
-//         nrInStock: newProduct.nrInStock,
-//       });
-//     next();
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// }
 
 module.exports = { getAllProducts, addProduct };
