4a7f2fef70af208cbd44a95e7a378b08052b0eac
Fredrik Malmborg <fredrik.malmborg@medieinstitutet>
2020-06-03 14:20:46 +0200

    moved layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import Theme from './MuiTheme'
 
 import UserContextProvider from "./contexts/UserContext"
---
+++ client/src/components/Layout/Layout.tsx
@@ -0,0 +1,57 @@
+import React, { CSSProperties } from 'react'
+import Header from '../Header/Header';
+import Home from '../home/Home';
+import Footer from '../Footer/Footer';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import CheckOut from './checkout/CheckOut';
+import ProductPage from '../product/ProductPage';
+import AdminLayout from '../admin/AdminLayout';
+import CheckoutWrapper from '../checkout/CheckoutWrapper';
+
+import { UserContext } from "../../contexts/UserContext"
+import { Container } from '@material-ui/core';
+
+import useStyles from "./LayoutStyles"
+
+const Layout = () => {
+    const classes = useStyles()
+
+    return (
+
+        <UserContext.Consumer>
+            {userContext => (
+                <BrowserRouter>
+
+                    <Header />
+                    <Container className={classes.root}>
+
+
+                        <Switch>
+                            <Route exact path={userContext.admin ? "/home" : "/"}>
+                                <Home />
+                            </Route>
+                            <Route exact path={userContext.admin ? "/" : ""}>
+                                <AdminLayout userContext={userContext} />
+                            </Route>
+                            <Route path={userContext.loggedIn ? "/checkout" : ""}>
+                                <CheckoutWrapper />
+                            </Route>
+                            <Route exact path="/product">
+                                <div><h2>Select a product</h2></div>
+                            </Route>
+                            <Route path="/product/:id">
+                                <ProductPage />
+                            </Route>
+                        </Switch>
+
+
+                    </Container>
+                    <Footer />
+                </BrowserRouter>
+
+            )}
+        </UserContext.Consumer>
+    );
+}
+
+export default Layout;
