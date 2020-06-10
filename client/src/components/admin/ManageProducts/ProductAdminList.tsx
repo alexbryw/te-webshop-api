e4444f80378c2c2717d9e780e00d4227037a0b5b
Markus Karlsson <markuskarlsson__@hotmail.com>
2020-06-10 16:37:34 +0200

    in progress

--- client/src/components/admin/ManageProducts/ProductAdminList.tsx
+++ client/src/components/admin/ManageProducts/ProductAdminList.tsx
@@ -1,69 +1,77 @@
-import React,{CSSProperties} from 'react'
-import { Product } from '../../../interfaces/interfaces'
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import EditItem from './EditItem'
-import IconButton from '@material-ui/core/IconButton'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import React, { CSSProperties } from "react";
+import { Product } from "../../../interfaces/interfaces";
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import EditItem from "./EditItem";
+import IconButton from "@material-ui/core/IconButton";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
-interface Props{
-    productContext: any
-    itemData: any
-    arrayIndex: number
-    handleSubmit: any
-    delete: any
+interface Props {
+  productContext: any;
+  itemData: any;
+  arrayIndex: number;
+  handleSubmit: any;
+  delete: any;
 }
 
 interface State {
-    toggleItem: boolean,
-    deleted: boolean
+  toggleItem: boolean;
+  deleted: boolean;
 }
 
 export default class ProductAdminList extends React.Component<Props, State> {
-    constructor(props:Props){
-        super(props)
-        this.state = {
-            toggleItem: false,
-            deleted: false
-        }   
-    }
-    // Sends a state that disables the EditItem view
-    isDeleted = () =>{
-        this.setState({deleted: true})
-    }
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      toggleItem: false,
+      deleted: false,
+    };
+  }
+  // Sends a state that disables the EditItem view
+  isDeleted = () => {
+    this.setState({ deleted: true });
+  };
 
-    //toggles the EditItem view
-    toggle = () => { this.setState({toggleItem: !this.state.toggleItem})}
+  //toggles the EditItem view
+  toggle = () => {
+    this.setState({ toggleItem: !this.state.toggleItem });
+  };
 
-    render(){
-        return(
-            <Card style={adminListStyle} variant="outlined">
-                <Typography onClick={this.toggle} color={this.state.deleted?"error":"inherit"}>
-                    <IconButton>
-                        {this.state.toggleItem?  <ExpandLessIcon/> : <ExpandMoreIcon/>}
-                    </IconButton>
-                    {'Prod# ' + this.props.itemData._id + " - " + this.props.itemData.title}
-                    {this.state.deleted? " - [Raderad]":""}
-                </Typography>
+  render() {
+    return (
+      <Card style={adminListStyle} variant="outlined">
+        <Typography
+          onClick={this.toggle}
+          color={this.state.deleted ? "error" : "inherit"}
+        >
+          <IconButton>
+            {this.state.toggleItem ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </IconButton>
+          {"Prod# " +
+            this.props.itemData._id +
+            " - " +
+            this.props.itemData.title}
+          {this.state.deleted ? " - [Raderad]" : ""}
+        </Typography>
 
-                    {this.state.toggleItem?
-                        <EditItem 
-                            productContext={this.props.productContext}
-                            itemData={this.props.itemData} 
-                            arrayIndex={this.props.arrayIndex} 
-                            delete={this.props.delete}
-                            handleSubmit={this.props.handleSubmit}
-                            isDeleted={this.isDeleted}
-                            deleted={this.state.deleted}
-                        /> 
-                    :null}
-            </Card>
-        )
-    }
+        {this.state.toggleItem ? (
+          <EditItem
+            productContext={this.props.productContext}
+            itemData={this.props.itemData}
+            arrayIndex={this.props.arrayIndex}
+            delete={this.props.delete}
+            handleSubmit={this.props.handleSubmit}
+            isDeleted={this.isDeleted}
+            deleted={this.state.deleted}
+          />
+        ) : null}
+      </Card>
+    );
+  }
 }
 
-const adminListStyle:CSSProperties = {
-    cursor: 'pointer',
-    padding: '1em'
-}
+const adminListStyle: CSSProperties = {
+  cursor: "pointer",
+  padding: "1em",
+};
--- client/src/contexts/ProductContext.tsx
+++ client/src/contexts/ProductContext.tsx
@@ -3,13 +3,13 @@ import React, { createContext, Component } from "react";
 import { Product } from "../interfaces/interfaces";
 import { appendFile } from "fs";
 
-interface Props { }
+interface Props {}
 interface State {
   products: Product[];
 
-  uploadFile:(file:any) => any
-  updateProduct:(product:any, productId: any) => any
-  postProduct:(product: any, file:any) => any
+  uploadFile: (file: any) => any;
+  updateProduct: (product: any, productId: any) => any;
+  postProduct: (product: any, file: any) => any;
   fetchProducts: () => any;
   fetchProduct: (id: string) => any;
   getCategories: () => any;
@@ -19,15 +19,15 @@ export const ProductContext = createContext<State>({
   products: [],
 
   updateProduct: () => {
-    return ""
+    return "";
   },
 
   uploadFile: () => {
-    return ""
+    return "";
   },
 
   postProduct: () => {
-    return ""
+    return "";
   },
 
   fetchProducts: () => {
@@ -39,7 +39,7 @@ export const ProductContext = createContext<State>({
 
   getCategories: () => {
     return [];
-  }
+  },
 });
 
 export class ProductContextProvider extends Component<Props, State> {
@@ -53,47 +53,44 @@ export class ProductContextProvider extends Component<Props, State> {
       postProduct: this.postProduct,
       fetchProducts: this.fetchProducts,
       fetchProduct: this.fetchProduct,
-      getCategories: this.getCategories
+      getCategories: this.getCategories,
     };
   }
 
   getCategories = async () => {
-    const products = await this.fetchProducts("")
-    let categories: string[] = [""]
+    const products = await this.fetchProducts("");
+    let categories: string[] = [""];
 
     products.forEach((product: any) => {
       product.category.forEach((cate: string) => {
-        const found = categories.find(x => x === cate)
+        const found = categories.find((x) => x === cate);
 
-        if (!found) categories.push(cate)
+        if (!found) categories.push(cate);
         console.log(cate, found);
       });
     });
     console.log(categories);
 
-    return categories
-  }
+    return categories;
+  };
 
   fetchProducts = async (filter?: String) => {
     let filterURL: string;
 
     if (filter === "") {
-      filterURL = "http://localhost:9000/api/products/"
-
+      filterURL = "http://localhost:9000/api/products/";
     } else {
-      filterURL = "http://localhost:9000/api/products/category/" + filter 
+      filterURL = "http://localhost:9000/api/products/category/" + filter;
     }
 
-    const products = await fetch(filterURL,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    )
+    const products = await fetch(filterURL, {
+      method: "GET",
+      credentials: "include",
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-       
+
         return data;
       });
     return products;
@@ -108,125 +105,123 @@ export class ProductContextProvider extends Component<Props, State> {
       .then((data) => {
         console.log(data);
         return data;
-      });}
+      });
+  };
 
+  uploadFile = async (file: any) => {
+    console.log(file.size);
+    const fd = new FormData();
+    fd.append("image", file);
+    console.log(fd);
+    // send `POST` request
+    const uploadedFile = await fetch("http://localhost:9000/api/files/", {
+      method: "POST",
+      body: fd,
+    });
+    if (!uploadedFile) {
+      console.log("this file is NOT uploading 🍊");
+    }
+    const newFile = await uploadedFile.json();
 
-    uploadFile = async (file:any) => {
-      console.log(file.size)
-      const fd = new FormData();
-      fd.append( 'image', file);
-      console.log(fd)
-      // send `POST` request
-      const uploadedFile = await fetch ('http://localhost:9000/api/files/', {
-          method: 'POST',
-          body: fd
-      })
-      if(!uploadedFile){
-        console.log('this file is NOT uploading 🍊')
-      }
-      const newFile = await uploadedFile.json()
-    
+    // .then(res => res.json())
+    //.then(save => save.json())
 
-      // .then(res => res.json())
-      //.then(save => save.json())
-      
-      // .then(json => console.log(json))
-      // .catch(err => console.error(err));
+    // .then(json => console.log(json))
+    // .catch(err => console.error(err));
 
-      if(!['image/jpeg', 'image/png', 'image/jpg'].includes(file.type)) {
-          console.log('Only images are allowed.');
-          return;
-      }
-      console.log(newFile)
-      console.log(newFile._id)
-      // check file size (< 2MB)
-      if(file.size > 2 * 1024 * 1024) {
-          console.log('File must be less than 2MB.');
-          return;
-      }
-      return newFile
-    }    
+    if (!["image/jpeg", "image/png", "image/jpg"].includes(file.type)) {
+      console.log("Only images are allowed.");
+      return;
+    }
+    console.log(newFile);
+    console.log(newFile._id);
+    // check file size (< 2MB)
+    if (file.size > 2 * 1024 * 1024) {
+      console.log("File must be less than 2MB.");
+      return;
+    }
+    return newFile;
+  };
 
-  postProduct = async (product: any, file:any) => {
-    console.log('***********************')
-    console.log(product)
-    console.log(file)
-    console.log('***********************')
-    const getUploadedFile = await this.uploadFile(file)
-    console.log(getUploadedFile._id)
-    product.file = getUploadedFile._id
-    console.log(product)
+  postProduct = async (product: any, file: any) => {
+    console.log("***********************");
+    console.log(product);
+    console.log(file);
+    console.log("***********************");
+    const getUploadedFile = await this.uploadFile(file);
+    console.log(getUploadedFile._id);
+    product.file = getUploadedFile._id;
+    console.log(product);
     // const fd = new FormData(file);
     //       fd.append('product', product + 'uploadFile', file._id)
-          //fd.append('product', product)
-  
+    //fd.append('product', product)
 
     const completeProduct = await fetch("http://localhost:9000/api/products", {
       method: "POST",
       credentials: "include",
       headers: {
-          "Content-Type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        "file": product.file,
-        "title": product.title,
-        "description": product.description,
-        "price": product.price,
-        "category": product.category,
-        "nrInStock": product.nrInStock
-      })
-    }) 
-    const returnedProduct = await completeProduct.json()
-    console.log(returnedProduct)
+        file: product.file,
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        category: product.category,
+        nrInStock: product.nrInStock,
+      }),
+    });
+    const returnedProduct = await completeProduct.json();
+    console.log(returnedProduct);
 
     //TODO ERROR Controll
 
-      // .then((res) => res.json())
-      // .then((json) => {
-      //   console.log('************************',json);
-      //  // console.log(this.uploadFile, '******************* THIS IS THE IMAGE****************')
-      //   return json;
-      // });
-      // return ;
-    
+    // .then((res) => res.json())
+    // .then((json) => {
+    //   console.log('************************',json);
+    //  // console.log(this.uploadFile, '******************* THIS IS THE IMAGE****************')
+    //   return json;
+    // });
+    // return ;
   };
 
   updateProduct = async (product: any, productId: any) => {
-    console.log("from updateproduct", product, productId)
+    console.log("from updateproduct", product, productId);
 
-      const updatedProduct = await fetch("http://localhost:9000/api/products/" + productId, {
-          method: "PUT",
-          credentials: "include",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title: product.title,
-            description: product.description,
-            price: product.price,
-            category: product.category,
-            nrInStock: product.nrInStock
-          })
-      })
-          .then((response) => response.json())
-          .then((data) => {
-              console.log(data);
-              return data
-          })
+    const updatedProduct = await fetch(
+      "http://localhost:9000/api/products/" + productId,
+      {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: product.title,
+          description: product.description,
+          price: product.price,
+          category: product.category,
+          nrInStock: product.nrInStock,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return data;
+      });
 
-      return updatedProduct
-  }
+    return updatedProduct;
+  };
 
-
-
-//   async componentDidMount(){
-//     const data = await this.uploadFile
-//     console.log("from product Context ****HELLO****")
-//     console.log(data)
-// }
+  //   async componentDidMount(){
+  //     const data = await this.uploadFile
+  //     console.log("from product Context ****HELLO****")
+  //     console.log(data)
+  // }
 
   textLogger = (text: String): void => {
-    console.log(text, 'this is the text? form ProductContext');
+    console.log(text, "this is the text? form ProductContext");
   };
 
   render() {
