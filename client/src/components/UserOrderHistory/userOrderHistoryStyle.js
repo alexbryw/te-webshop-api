0b9e7f8954c506a5722a83eb241b3b79b32fb843
Claudia Aranda <claudia_15_@hotmail.com>
2020-06-12 16:24:39 +0200

    css

--- client/src/components/UserOrderHistory/userOrderHistoryStyle.js
+++ client/src/components/UserOrderHistory/userOrderHistoryStyle.js
@@ -3,8 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
     recipteList: {
-        border: '0.1rem dashed #558B2F', 
-        marginBottom: '0.6rem'
+        border: '0.1rem solid #2c393f', 
+        marginBottom: '0.6rem',
+        backgroundColor: '#2c393f35'
     },
     flex: {
         display: "flex"
